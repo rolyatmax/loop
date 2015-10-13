a85f8269a5d637950f1bc185bcae5879a3a80c44
Taylor Baldwin <taylorbaldwin@gmail.com>
2015-10-13 12:12:33 -0400

    add comment re: require in tests and remove stop function

--- tests/test-loop.js
+++ tests/test-loop.js
@@ -2,7 +2,8 @@
 
 jest.dontMock('../src/loop');
 
-let {register, stop, clear} = require('../src/loop');
+// must use `require` as `import` is always hoisted
+let {register, clear} = require('../src/loop');
 
 
 describe('loop register', function() {
