a0792bfa1745c3a3ed451fe7b772d667606a98ee
Taylor Baldwin <taylorbaldwin@gmail.com>
2016-06-26 17:41:16 -0400

    fix rAF

--- src/rAF.js
+++ src/rAF.js
@@ -1,2 +1,3 @@
 // using this as a module so we can mock this when testing
-export default {requestAnimationFrame, cancelAnimationFrame};
+export const requestAnimationFrame = (...args) => window.requestAnimationFrame(...args);
+export const cancelAnimationFrame = (...args) => window.cancelAnimationFrame(...args);
